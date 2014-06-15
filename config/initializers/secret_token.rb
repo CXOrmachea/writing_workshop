dffe10ca53bb70a2a4a912f93cb6ce7a93205aad
CXOrmachea <CXOrmachea@gmail.com>
2014-06-15 15:09:14 -0700

    initial commit with devise stuff set up

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WritingWorkshop::Application.config.secret_token = '05867ab034a3568096f1a48d6a7ad1368c0dd57c77e58eab2267dd1bf18c74461989ae5b37e4fb6e39ae6c3dd7e97168851e4312aa1e6d4f1e09f915599c4d6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+WritingWorkshop::Application.config.session_store :cookie_store, key: '_writing_workshop_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# WritingWorkshop::Application.config.session_store :active_record_store
